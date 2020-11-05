7b7520e07c00c26fe8c1086ec1cb05da297d0a6a
kira2406 <kush.p030.24@gmail.com>
2020-11-05 23:58:52 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmRc-XR01vJG0my0u2hQpYNBBmtyQr8qM",
+  authDomain: "pokereigns.firebaseapp.com",
+  databaseURL: "https://pokereigns.firebaseio.com",
+  projectId: "pokereigns",
+  storageBucket: "pokereigns.appspot.com",
+  messagingSenderId: "630420974502",
+  appId: "1:630420974502:web:5886d80807232170c2bd67",
+  measurementId: "G-Z1DH7YD8BZ",
+};
